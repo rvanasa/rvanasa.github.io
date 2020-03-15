23b3d9ffe6bb964626fad4fed08a8a9eeff43f4e
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-03-15 12:14:07 -0600

    Updates

--- precache-manifest.24de262ef55b0a4cd40c86622e49032c.js
+++ precache-manifest.24de262ef55b0a4cd40c86622e49032c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cfe7a81db9452fd43c26add73726cef1",
+    "revision": "4653b6ce4d30d48c5f9f17c55652a3b4",
     "url": "/index.html"
   },
   {
-    "revision": "4b9c603b9e3c14ff94f5",
-    "url": "/static/css/main.f29507f7.chunk.css"
+    "revision": "469c081b2ae7aa50bbd7",
+    "url": "/static/css/main.ff9c4188.chunk.css"
   },
   {
     "revision": "b475bc2f1bc6eefb6df7",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.35421ce2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4b9c603b9e3c14ff94f5",
+    "revision": "469c081b2ae7aa50bbd7",
     "url": "/static/js/main.d03be931.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2aa0a1d1a6c58bc50b93667794d97317.js"
+  "/precache-manifest.24de262ef55b0a4cd40c86622e49032c.js"
 );
 
 self.addEventListener('message', (event) => {
